c925f0fca962d36ac461b488f6b258e7c5ee40b7
Alexander Tkach <a13xander.tk@gmail.com>
2023-04-14 10:52:22 +0300

    Add configurable query params

--- aparts.py
+++ aparts.py
@@ -15,10 +15,14 @@ from dataclasses import dataclass
 BOT_API_KEY = os.environ['BOT_API_KEY']
 CHAT_ID = os.environ['CHAT_ID']
 CITY = os.environ['CITY']
+QUERY_PARAMS = os.environ.get('QUERY_PARAMS', None)
 POLL_INTERVAL = int(os.environ['POLL_INTERVAL']) # seconds
 REDIS_HOST = os.environ['REDIS_HOST']
 
 QUERY_URL = f'https://www.olx.ua/d/uk/nedvizhimost/kvartiry/dolgosrochnaya-arenda-kvartir/{CITY}/?search[order]=created_at:desc'
+if QUERY_PARAMS is not None:
+    QUERY_URL += '&{QUERY_PARAMS}'
+
 SEND_MESSAGE_URL_FORMAT = f'https://api.telegram.org/bot{BOT_API_KEY}/sendMessage?chat_id={CHAT_ID}&text={{text}}'
 MAX_REFRESH_TIME_REDIS_KEY = f'MRT_{CITY}_{CHAT_ID}'
 
